c768d319fbcdd5f97bc5cc8f579b2c3a7cfc0530
Sergey <maraev@mail.ru>
2020-11-25 19:33:35 +0300

    Закончен основной рефакторинг

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ const uglify = require('gulp-uglify-es').default;
 const concat = require('gulp-concat');
 const imagemin = require('gulp-imagemin');
 const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const cleancss = require('gulp-clean-css');
 
 const sourcemap = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
@@ -39,25 +41,7 @@ let path = {
   clean: './' + project_folder + '/'
 }
 
-/* gulp.task('html', function () {
-  return src(path.src.html)
-    .pipe(dest(path.build.html))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('scss', function () {
-  return src(path.src.css)
-    .pipe(sourcemap.init())
-    .pipe(sass({
-      outputStyle: "compressed"
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(sourcemap.write('./'))
-    .pipe(dest(path.build.css))
-    .pipe(browserSync.stream());
-});
+/* gulp.task('html', );
 
 gulp.task('css_concat', function () {
   return src([
@@ -81,17 +65,7 @@ gulp.task('js_concat', function () {
     .pipe(browserSync.stream());
 });
 
-gulp.task('img', function () {
-  return src(path.src.img)
-    .pipe(imagemin({
-      progressive: true,
-      svgoPlugins: [{ removeViewBox: false }],
-      interlaced: true,
-      optimizationLevel: 3
-    }))
-    .pipe(dest(path.build.img))
-    .pipe(browserSync.stream());
-}) */
+*/
 
 function browsersync() {
   browserSync.init({
@@ -104,6 +78,12 @@ function browsersync() {
   });
 };
 
+function html () {
+  return src(path.src.html)
+    .pipe(dest(path.build.html))
+    .pipe(browserSync.stream());
+}
+
 function scripts() {
   return src(path.src.js)
     .pipe(concat('main.min.js'))
@@ -112,11 +92,48 @@ function scripts() {
     .pipe(browserSync.stream());
 }
 
+function styles () {
+  return src(path.src.css)
+    .pipe(sourcemap.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 versions'],
+      grid: true
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sourcemap.write('./'))
+    .pipe(dest(path.build.css))
+    .pipe(browserSync.stream());
+}
+
+function images () {
+  return src(path.src.img)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      interlaced: true,
+      optimizationLevel: 3
+    }))
+    .pipe(dest(path.build.img))
+    .pipe(browserSync.stream());
+}
+
 function startwatch() {
+  watch(path.src.html, html);
   watch([path.src.js, '!#src/js/**/*.min.js'], scripts);
+  watch([path.src.css, '!#src/css/**/*.min.css'], styles);
+  watch(path.src.img, images);
+  watch(path.src.html).on('change', browserSync.reload);
 }
 
 exports.browsersync = browsersync;
+exports.html = html;
 exports.scripts = scripts;
+exports.styles = styles;
+exports.images = images;
 
-exports.default = parallel(scripts, browsersync, startwatch);
+exports.default = parallel(html, scripts, styles, browsersync, startwatch);
